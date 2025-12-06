8816c3df91e708873b7c69887e31d9af8840736b
Md Robiul Islam <mdrobiulislam1479@gmail.com>
2025-12-06 18:41:55 +0600

    create all route

---
+++ src/routes/router.jsx
@@ -0,0 +1,131 @@
+import { createBrowserRouter } from "react-router";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    errorElement: <ErrorPage />,
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "all-tickets", element: <AllTickets /> },
+      {
+        path: "ticket/:id",
+        element: (
+          <ProtectedRoute>
+            <TicketDetails />
+          </ProtectedRoute>
+        ),
+      },
+    ],
+  },
+
+  // Auth
+  {
+    path: "/login",
+    element: (
+      <AuthLayout>
+        <Login />
+      </AuthLayout>
+    ),
+  },
+  {
+    path: "/register",
+    element: (
+      <AuthLayout>
+        <Register />
+      </AuthLayout>
+    ),
+  },
+
+  // Dashboard
+  {
+    path: "/dashboard",
+    element: (
+      <ProtectedRoute>
+        <DashboardLayout />
+      </ProtectedRoute>
+    ),
+    children: [
+      // User
+      { path: "profile", element: <UserProfile /> },
+      { path: "my-bookings", element: <MyBookedTickets /> },
+      { path: "transactions", element: <TransactionHistory /> },
+
+      // Vendor
+      {
+        path: "vendor/profile",
+        element: (
+          <VendorRoute>
+            <VendorProfile />
+          </VendorRoute>
+        ),
+      },
+      {
+        path: "vendor/add-ticket",
+        element: (
+          <VendorRoute>
+            <AddTicket />
+          </VendorRoute>
+        ),
+      },
+      {
+        path: "vendor/my-tickets",
+        element: (
+          <VendorRoute>
+            <MyAddedTickets />
+          </VendorRoute>
+        ),
+      },
+      {
+        path: "vendor/requests",
+        element: (
+          <VendorRoute>
+            <RequestedBookings />
+          </VendorRoute>
+        ),
+      },
+      {
+        path: "vendor/revenue",
+        element: (
+          <VendorRoute>
+            <RevenueOverview />
+          </VendorRoute>
+        ),
+      },
+
+      // Admin
+      {
+        path: "admin/profile",
+        element: (
+          <AdminRoute>
+            <AdminProfile />
+          </AdminRoute>
+        ),
+      },
+      {
+        path: "admin/manage-tickets",
+        element: (
+          <AdminRoute>
+            <ManageTickets />
+          </AdminRoute>
+        ),
+      },
+      {
+        path: "admin/manage-users",
+        element: (
+          <AdminRoute>
+            <ManageUsers />
+          </AdminRoute>
+        ),
+      },
+      {
+        path: "admin/advertise",
+        element: (
+          <AdminRoute>
+            <AdvertiseTickets />
+          </AdminRoute>
+        ),
+      },
+    ],
+  },
+]);
