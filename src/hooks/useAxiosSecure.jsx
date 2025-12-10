d77abd6a0b4bc4f22ffc00bba357f54914196bbc
Md Robiul Islam <mdrobiulislam1479@gmail.com>
2025-12-10 10:13:47 +0600

    create useAxiosSecure hook

---
+++ src/hooks/useAxiosSecure.jsx
@@ -0,0 +1,51 @@
+import { use, useEffect } from "react";
+import { useNavigate } from "react-router";
+import axios from "axios";
+import { AuthContext } from "../context/AuthContext";
+
+const axiosInstance = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
+  withCredentials: true,
+});
+
+const useAxiosSecure = () => {
+  const { user, signOutUser, loading } = use(AuthContext);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!loading && user?.accessToken) {
+      // Add request interceptor
+      const requestInterceptor = axiosInstance.interceptors.request.use(
+        (config) => {
+          config.headers.Authorization = `Bearer ${user.accessToken}`;
+          return config;
+        }
+      );
+
+      // Add response interceptor
+      const responseInterceptor = axiosInstance.interceptors.response.use(
+        (res) => res,
+        (err) => {
+          if (err?.response?.status === 401 || err?.response?.status === 403) {
+            signOutUser()
+              .then(() => {
+                console.log("Logged out successfully.");
+              })
+              .catch(console.error);
+            navigate("/login");
+          }
+          return Promise.reject(err);
+        }
+      );
+
+      // Cleanup to prevent multiple interceptors on re-renders
+      return () => {
+        axiosInstance.interceptors.request.eject(requestInterceptor);
+        axiosInstance.interceptors.response.eject(responseInterceptor);
+      };
+    }
+  }, [user, loading, signOutUser, navigate]);
+
+  return axiosInstance;
+};
+export default useAxiosSecure;
--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,17 @@ import { RouterProvider } from "react-router";
 import { router } from "./routes/router";
 import AuthProvider from "./context/AuthProvider";
 import { ToastContainer } from "react-toastify";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <AuthProvider>
-      <RouterProvider router={router} />
-    </AuthProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <RouterProvider router={router} />
+      </AuthProvider>
+    </QueryClientProvider>
     <ToastContainer position="top-center" autoClose={3000} />
   </StrictMode>
 );
